5f9ae09361cfc2eea555921f72243ef212075998
MANSI GAUTAM <62936067+mansigautam777@users.noreply.github.com>
2021-07-07 20:23:53 +0530

    splashcreen added

--- chart_app/chart_app/lib/main.dart
+++ chart_app/chart_app/lib/main.dart
@@ -1,7 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:chart_app/screens/splashscreen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
+import 'package:chart_app/services/Authservices.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+
+
+
+  // This needs to be called before any Firebase services can be used
+  WidgetsFlutterBinding.ensureInitialized();
+  FirebaseApp app =
+  await Firebase.initializeApp().catchError((error) => print(error));
+
+  runApp(
+    ChangeNotifierProvider<AuthService>(
+      create: (context) => AuthService(),
+      child: MyApp(),
+    ),
+  );
+}
 
 class MyApp extends StatelessWidget {
   @override
